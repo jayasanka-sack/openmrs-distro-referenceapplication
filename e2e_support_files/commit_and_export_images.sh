7b5ff3e337b3db8cdd466a0191904bb643620411
Jayasanka <jayasanka.sack@gmail.com>
2023-10-23 18:25:53 -0700

    Test commit

--- e2e_support_files/commit_and_export_images.sh
+++ e2e_support_files/commit_and_export_images.sh
@@ -9,7 +9,7 @@ gateway_container_id=$(docker ps --filter "name=gateway" --format "{{.ID}}")
 
 docker commit $frontend_container_id frontend
 docker commit $gateway_container_id gateway
-docker commit $backend_container_id db
+docker commit $backend_container_id backend
 docker commit $db_container_id db
 
 docker save frontend gateway backend db > e2e_release_env_images.tar
