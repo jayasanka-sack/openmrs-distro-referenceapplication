afe5d88c7b2725c7d9fd0e200819d1847729cfd7
Jayasanka <jayasanka.sack@gmail.com>
2023-10-24 09:30:31 -0700

    Revert

--- e2e_support_files/read-versions.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Define the file path
-file_path="frontend/spa-build-config.json"
-version=$(grep '"@openmrs/esm-patient-registration-app":' "$file_path" | awk -F'"' '{print $4}')
-echo "Version: $version"
